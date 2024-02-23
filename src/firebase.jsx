9366ec93439a6c05df58c056ecc4ea5072b8e222
sameerbutt28 <sameerofficial929@gmail.com>
2024-02-23 20:24:00 +0500

    UPDATING

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -23,7 +23,7 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 
-const auth = getAuth();
+ export const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
