7c728224aa5152f9a0ed4f6a997b966eced39000
sameerbutt28 <sameerofficial929@gmail.com>
2024-02-23 19:33:53 +0500

    INITIALIZING USESTATE IN SIGNIN.JSX

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const SignIn = () => {
+    const [email, setEmail] =useState('');
+    const [password, setPassword] =useState('');
   return (
     <div className='sign-in'>
        <form >
-<input type="email" placeholder='Enter your Email' />
-<input type="password" placeholder='Enter your Password' />
+<input type="email" placeholder='Enter your Email' value={email} />
+<input type="password" placeholder='Enter your Password' value={password} />
 
        </form>
     </div>
