f952bc558142ebcab04b7ab52db555f1c9ad1733
sameerbutt28 <sameerofficial929@gmail.com>
2024-02-23 19:30:42 +0500

    UPDATING SignIn Page

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>
-       
+    <div className='sign-in'>
+       <form >
+<input type="email" placeholder='Enter your Email' />
+<input type="password" placeholder='Enter your Password' />
+
+       </form>
     </div>
   )
 }
