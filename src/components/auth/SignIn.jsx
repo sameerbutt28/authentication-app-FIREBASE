37e0f9c2d936603a89359ea5b0db7cb8f90f93a9
sameerbutt28 <sameerofficial929@gmail.com>
2024-02-23 20:15:59 +0500

    ADDING AUTHENTICATION LOGIC

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,25 +1,38 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 const SignIn = () => {
-    const [email, setEmail] =useState('');
-    const [password, setPassword] =useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const SignIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
-    <div className='sign-in'>
-       <form >
-<input type="email" placeholder='Enter your Email' value={email} 
-onChange={(e)=>
-setEmail(e.target.value)
-}
-/>
-<input type="password" placeholder='Enter your Password' value={password} 
-onChange={(e)=>
-    setPassword(e.target.value)
-    }
-/>
-
-       </form>
+    <div className="sign-in">
+      <form onSubmit={SignIn}>
+        <input
+          type="email"
+          placeholder="Enter your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter your Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">LogIn</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
