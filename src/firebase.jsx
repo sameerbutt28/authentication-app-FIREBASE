7cd1ba47b8350813aa044c732cb40767afa55f79
sameerbutt28 <sameerofficial929@gmail.com>
2024-02-24 18:40:39 +0500

    FIREBASE DATABASE INTEGRATION

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics"; 
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-
+import { getDatabase, ref, set } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,19 +11,31 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyBOuc4-OUCPmTVnc15Uu18bZtsC20ncQDI",
   authDomain: "auhentication-app-a896e.firebaseapp.com",
-  projectId: "auhentication-app-a896e", 
+  projectId: "auhentication-app-a896e",
   storageBucket: "auhentication-app-a896e.appspot.com",
   messagingSenderId: "1037162023200",
   appId: "1:1037162023200:web:5d8fa67ce2b154fe49af45",
-  measurementId: "G-T43D1W3XVJ"
+  measurementId: "G-T43D1W3XVJ",
 };
 
-// Initialize Firebase
+//database code
+function writeUserData(userId, name, email, imageUrl) {
+  const db = getDatabase(); //not to call getdata base function mulliple times we make a vatiable of it.
+  const reference = ref(db, "users/" + userId); // we ahve to add users so db and passing the path.
+  // Initialize Firebase
+
+  set(reference, {
+    username: name,
+    email: email,
+    profile_picture: imageUrl,
+  });
+}
+writeUserData("sammy28", "sameer", "buttjee@gmail.com", "myImageUrl");
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-
- export const auth = getAuth();
+export const auth = getAuth();
 onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
