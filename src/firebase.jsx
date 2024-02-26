41c51c657de376e92b8f0354c301fda232b4bd92
sameerbutt28 <sameerofficial929@gmail.com>
2024-02-26 23:49:17 +0500

    REFACTORING

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -11,11 +11,12 @@ import { getDatabase, ref, set } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyBOuc4-OUCPmTVnc15Uu18bZtsC20ncQDI",
   authDomain: "auhentication-app-a896e.firebaseapp.com",
+  databaseURL: "https://auhentication-app-a896e-default-rtdb.firebaseio.com",
   projectId: "auhentication-app-a896e",
   storageBucket: "auhentication-app-a896e.appspot.com",
   messagingSenderId: "1037162023200",
   appId: "1:1037162023200:web:5d8fa67ce2b154fe49af45",
-  measurementId: "G-T43D1W3XVJ",
+  measurementId: "G-T43D1W3XVJ"
 };
 
 //database code
